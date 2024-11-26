eb7fab810be28d379b8342306865d9ecb25fe61e
Carlos Peña <cgabriel2303@gmail.com>
2024-11-26 16:34:49 -0300

    fix route

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from .views import register_view, login_view,home,logout_view,add_product,produc
 urlpatterns = [
     path('home/', home, name='home'),
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
+    path('', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('add-product/', add_product, name='add_product'),
     path('products/', product_list, name='product_list'),
